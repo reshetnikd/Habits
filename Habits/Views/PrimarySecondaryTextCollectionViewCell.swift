86a39b8f479b5c21bc246a1582aafe8b00498b7e
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-05 15:57:20 +0300

    Habit Collection View Controller Implementation

---
+++ Habits/Views/PrimarySecondaryTextCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PrimarySecondaryTextCollectionViewCell.swift
+//  Habits
+//
+//  Created by Dmitry Reshetnik on 05.05.2021.
+//
+
+import UIKit
+
+class PrimarySecondaryTextCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var primaryTextLabel: UILabel!
+    @IBOutlet var secondaryTextLabel: UILabel!
+}
