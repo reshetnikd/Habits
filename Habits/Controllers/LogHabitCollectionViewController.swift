43ab11345bed0613a3c84d77b83be99114c6ca03
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-11 16:53:31 +0300

    Log Habits

--- Habits/Controllers/LogHabitCollectionViewController.swift
+++ Habits/Controllers/LogHabitCollectionViewController.swift
@@ -45,4 +45,14 @@ class LogHabitCollectionViewController: HabitCollectionViewController {
             }
         }
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let item = dataSource.itemIdentifier(for: indexPath) else {
+            return
+        }
+        
+        let loggedHabit = LoggedHabit(userID: Settings.shared.currentUser.id, habitName: item.habit.name, timestamp: Date())
+        
+        LogHabitRequest(trackedEvent: loggedHabit).send { _ in }
+    }
 }
