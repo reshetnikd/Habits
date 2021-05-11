a48b20613c804a80186f409557c72841e6ccc27f
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-11 17:56:50 +0300

    Leaderboard Cell

---
+++ Habits/Views/LeaderboardHabitCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LeaderboardHabitCollectionViewCell.swift
+//  Habits
+//
+//  Created by Dmitry Reshetnik on 11.05.2021.
+//
+
+import UIKit
+
+class LeaderboardHabitCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var habitNameLabel: UILabel!
+    @IBOutlet var leaderLabel: UILabel!
+    @IBOutlet var secondaryLabel: UILabel!
+}
