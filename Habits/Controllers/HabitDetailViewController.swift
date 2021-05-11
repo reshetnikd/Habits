83ff64174cb60fe4307275e70ffdfc6ba628a12e
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-11 11:43:09 +0300

    Set Up the Habit Detail Scene

--- Habits/Controllers/HabitDetailViewController.swift
+++ Habits/Controllers/HabitDetailViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class HabitDetailViewController: UIViewController {
-
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var infoLabel: UILabel!
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
