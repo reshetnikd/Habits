009be5c644c0bbe500557799a924468b3d556170
reshetnikd <dmitry.reshetnik@icloud.com>
2021-05-12 12:35:37 +0300

    Apply Colors

--- Habits/Model/Color.swift
+++ Habits/Model/Color.swift
@@ -5,7 +5,7 @@
 //  Created by Dmitry Reshetnik on 05.05.2021.
 //
 
-import Foundation
+import UIKit
 
 struct Color {
     let hue: Double
@@ -20,3 +20,11 @@ extension Color: Codable {
         case brightness = "b"
     }
 }
+
+extension Color {
+    var uiColor: UIColor {
+        return UIColor(hue: CGFloat(hue), saturation: CGFloat(saturation), brightness: CGFloat(brightness), alpha: 1)
+    }
+}
+
+extension Color: Hashable { }
